36eb956a372e55abaff31f3e556f056b40b62019
Edgar <edgarpavlovsky@users.noreply.github.com>
2022-07-13 22:52:43 -0400

    more

--- python/tests/test_config.py
+++ python/tests/test_config.py
@@ -1,6 +1,7 @@
 import pytest
 from testfixtures import compare
 
+from solana.publickey import PublicKey
 from marginpy import MarginfiConfig, Environment
 
 class TestEnvironment():
@@ -14,6 +15,19 @@ class TestEnvironment():
 class TestMarginfiConfig():
 
     def test___init__(self):
-        config = MarginfiConfig(Environment.MAINNET)
+        config_mainnet = MarginfiConfig(Environment.MAINNET)
+        config_devnet = MarginfiConfig(Environment.DEVNET)
 
-        assert isinstance(config, MarginfiConfig)
+        assert isinstance(config_mainnet, MarginfiConfig)
+        assert isinstance(config_devnet, MarginfiConfig)
+
+    def test_handle_override(self):
+        overrides = {
+            "program_id": PublicKey("mfi5YpVKT1bAJbKv7h55c6LgoTsW3LvZyRm2k811XtK")
+        }
+        config_mainnet = MarginfiConfig(Environment.MAINNET, overrides)
+
+        compare(
+            config_mainnet.program_id,
+            PublicKey("mfi5YpVKT1bAJbKv7h55c6LgoTsW3LvZyRm2k811XtK")
+        )
