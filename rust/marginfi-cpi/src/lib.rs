c356a05c57b7a737c8f372ced14aa8b2aa00204f
man0s <95379755+losman0s@users.noreply.github.com>
2022-07-27 19:55:01 +0800

    upgrade to anchor-gen version skipping unimplemented 'Default's

--- rust/marginfi-cpi/src/lib.rs
+++ rust/marginfi-cpi/src/lib.rs
@@ -1,5 +1,34 @@
-#![feature(trivial_bounds)]
-
-anchor_gen::generate_cpi_crate!("src/idl.json");
-
+#[cfg(feature = "mainnet-beta")]
+anchor_lang::declare_id!("mrgnfD8pJKsw4AxCDquyUBjgABNEaZ79iTLgtov2Yff");
+#[cfg(not(feature = "mainnet-beta"))]
 anchor_lang::declare_id!("mf2tjVmwcxgNfscvVNdN9t2LZ8YwPkNQabeTzyYw2Hn");
+
+anchor_gen::generate_cpi_interface!(
+    idl_path = "src/idl.json",
+    zero_copy(
+        MarginfiGroup,
+        MarginfiAccount,
+        UTPAccountConfig,
+        MDecimal,
+        UTPConfig,
+        Bank,
+    ),
+    packed(
+        MarginfiGroup,
+        MarginfiAccount,
+        UTPAccountConfig,
+        Bank,
+        MDecimal
+    )
+);
+
+impl Default for state::MarginfiGroup {
+    fn default() -> Self {
+        Self {
+            admin: Default::default(),
+            bank: Default::default(),
+            paused: false,
+            reserved_space: [0; 384],
+        }
+    }
+}
