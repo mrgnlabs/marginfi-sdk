ca10f3fce5eadebc67bffc284d5901bddddad90c
Edgar <edgarpavlovsky@users.noreply.github.com>
2022-07-28 01:15:41 -0400

    reverting account fns

--- python/src/marginpy/utp/mango/account.py
+++ python/src/marginpy/utp/mango/account.py
@@ -95,25 +95,22 @@ class UtpMangoAccount(UtpAccount):
             self.config.program_id,
         )
 
-        if self._config.mango.group_pk is None:
-            raise Exception("Mango group cannot be none.")
-        else:
-            return make_activate_ix(
-                ActivateArgs(
-                    authority_seed=authority_seed.public_key,
-                    authority_bump=utp_authority_bump,
-                ),
-                ActivateAccounts(
-                    marginfi_account=self._marginfi_account.pubkey,
-                    marginfi_group=self._config.group_pk,
-                    authority=self._program.provider.wallet.public_key,
-                    mango_authority=utp_authority_pk,
-                    mango_account=utp_account_pk,
-                    mango_program=self._config.mango.program_id,
-                    mango_group=self._config.mango.group_pk,
-                ),
-                self._client.program_id,
-            )
+        return make_activate_ix(
+            ActivateArgs(
+                authority_seed=authority_seed.public_key,
+                authority_bump=utp_authority_bump,
+            ),
+            ActivateAccounts(
+                marginfi_account=self._marginfi_account.pubkey,
+                marginfi_group=self._config.group_pk,
+                authority=self._program.provider.wallet.public_key,
+                mango_authority=utp_authority_pk,
+                mango_account=utp_account_pk,
+                mango_program=self._config.mango.program_id,
+                mango_group=self._config.mango.group_pk,
+            ),
+            self._client.program_id,
+        )
 
     async def activate(self) -> TransactionSignature:
         """
@@ -188,36 +185,33 @@ class UtpMangoAccount(UtpAccount):
             mango_authority_pk,
         )
 
-        if self._config.mango.group_pk is None:
-            raise Exception("Mango group cannot be none.")
-        else:
-            return (
-                [
-                    *create_proxy_token_account_ixs,
-                    make_deposit_ix(
-                        args=DepositArgs(amount=ui_to_native(amount)),
-                        accounts=DepositAccounts(
-                            marginfi_account=self._marginfi_account.pubkey,
-                            marginfi_group=self._config.group_pk,
-                            signer=self._program.provider.wallet.public_key,
-                            margin_collateral_vault=self._marginfi_account.group.bank.vault,
-                            bank_authority=margin_bank_authority_pk,
-                            temp_collateral_account=proxy_token_account_key.public_key,
-                            mango_authority=mango_authority_pk,
-                            mango_account=self.address,
-                            mango_program=self._config.mango.program_id,
-                            mango_group=self._config.mango.group_pk,
-                            mango_cache=mango_group.cache,
-                            mango_root_bank=root_bank_pk,
-                            mango_node_bank=node_bank_pk,
-                            mango_vault=vault_pk,
-                        ),
-                        program_id=self._client.program_id,
-                        remaining_accounts=remaining_accounts,
+        return (
+            [
+                *create_proxy_token_account_ixs,
+                make_deposit_ix(
+                    args=DepositArgs(amount=ui_to_native(amount)),
+                    accounts=DepositAccounts(
+                        marginfi_account=self._marginfi_account.pubkey,
+                        marginfi_group=self._config.group_pk,
+                        signer=self._program.provider.wallet.public_key,
+                        margin_collateral_vault=self._marginfi_account.group.bank.vault,
+                        bank_authority=margin_bank_authority_pk,
+                        temp_collateral_account=proxy_token_account_key.public_key,
+                        mango_authority=mango_authority_pk,
+                        mango_account=self.address,
+                        mango_program=self._config.mango.program_id,
+                        mango_group=self._config.mango.group_pk,
+                        mango_cache=mango_group.cache,
+                        mango_root_bank=root_bank_pk,
+                        mango_node_bank=node_bank_pk,
+                        mango_vault=vault_pk,
                     ),
-                ],
-                proxy_token_account_key,
-            )
+                    program_id=self._client.program_id,
+                    remaining_accounts=remaining_accounts,
+                ),
+            ],
+            proxy_token_account_key,
+        )
 
     async def deposit(self, amount: float) -> TransactionSignature:
         """
@@ -262,29 +256,26 @@ class UtpMangoAccount(UtpAccount):
 
         remaining_accounts = await self.get_observation_accounts()
 
-        if self._config.mango.group_pk is None:
-            raise Exception("Mango group cannot be none.")
-        else:
-            return make_withdraw_ix(
-                WithdrawArgs(amount=ui_to_native(amount)),
-                WithdrawAccounts(
-                    marginfi_account=self._marginfi_account.pubkey,
-                    marginfi_group=self._config.group_pk,
-                    signer=self._program.provider.wallet.public_key,
-                    margin_collateral_vault=self._marginfi_account.group.bank.vault,
-                    mango_authority=mango_authority_pk,
-                    mango_account=self.address,
-                    mango_program=self._config.mango.program_id,
-                    mango_group=self._config.mango.group_pk,
-                    mango_cache=mango_group.cache,
-                    mango_root_bank=root_bank_pk,
-                    mango_node_bank=node_bank_pk,
-                    mango_vault=vault_pk,
-                    mango_vault_authority=mango_group.signer_key,
-                ),
-                self._client.program_id,
-                remaining_accounts,
-            )
+        return make_withdraw_ix(
+            WithdrawArgs(amount=ui_to_native(amount)),
+            WithdrawAccounts(
+                marginfi_account=self._marginfi_account.pubkey,
+                marginfi_group=self._config.group_pk,
+                signer=self._program.provider.wallet.public_key,
+                margin_collateral_vault=self._marginfi_account.group.bank.vault,
+                mango_authority=mango_authority_pk,
+                mango_account=self.address,
+                mango_program=self._config.mango.program_id,
+                mango_group=self._config.mango.group_pk,
+                mango_cache=mango_group.cache,
+                mango_root_bank=root_bank_pk,
+                mango_node_bank=node_bank_pk,
+                mango_vault=vault_pk,
+                mango_vault_authority=mango_group.signer_key,
+            ),
+            self._client.program_id,
+            remaining_accounts,
+        )
 
     async def withdraw(self, amount: float) -> TransactionSignature:
         """
@@ -386,41 +377,38 @@ class UtpMangoAccount(UtpAccount):
 
         remaining_accounts = await self.get_observation_accounts()
 
-        if self._config.mango.group_pk is None:
-            raise Exception("Mango group cannot be none.")
-        else:
-            return make_place_perp_order_ix(
-                PlacePerpOrderArgs(
-                    args=UtpMangoPlacePerpOrderArgs(
-                        side=side,
-                        price=int(native_price),
-                        max_base_quantity=int(native_quantity),
-                        max_quote_quantity=int(native_max_quote_quantity),
-                        client_order_id=client_order_id,
-                        order_type=order_type,
-                        reduce_only=reduce_only,
-                        expiry_timestamp=expiry_timestamp,
-                        limit=limit,
-                        expiry_type=expiry_type,
-                    )
-                ),
-                PlacePerpOrderAccounts(
-                    marginfi_account=self._marginfi_account.pubkey,
-                    marginfi_group=self._marginfi_account.group.pubkey,
-                    authority=self._program.provider.wallet.public_key,
-                    mango_authority=mango_authority_pk,
-                    mango_account=self.address,
-                    mango_program=self._config.mango.program_id,
-                    mango_group=self._config.mango.group_pk,
-                    mango_cache=mango_group.cache,
-                    mango_perp_market=perp_market.address,
-                    mango_bids=perp_market.bids_address,
-                    mango_asks=perp_market.asks_address,
-                    mango_event_queue=perp_market.event_queue_address,
-                ),
-                self._client.program_id,
-                remaining_accounts,
-            )
+        return make_place_perp_order_ix(
+            PlacePerpOrderArgs(
+                args=UtpMangoPlacePerpOrderArgs(
+                    side=side,
+                    price=int(native_price),
+                    max_base_quantity=int(native_quantity),
+                    max_quote_quantity=int(native_max_quote_quantity),
+                    client_order_id=client_order_id,
+                    order_type=order_type,
+                    reduce_only=reduce_only,
+                    expiry_timestamp=expiry_timestamp,
+                    limit=limit,
+                    expiry_type=expiry_type,
+                )
+            ),
+            PlacePerpOrderAccounts(
+                marginfi_account=self._marginfi_account.pubkey,
+                marginfi_group=self._marginfi_account.group.pubkey,
+                authority=self._program.provider.wallet.public_key,
+                mango_authority=mango_authority_pk,
+                mango_account=self.address,
+                mango_program=self._config.mango.program_id,
+                mango_group=self._config.mango.group_pk,
+                mango_cache=mango_group.cache,
+                mango_perp_market=perp_market.address,
+                mango_bids=perp_market.bids_address,
+                mango_asks=perp_market.asks_address,
+                mango_event_queue=perp_market.event_queue_address,
+            ),
+            self._client.program_id,
+            remaining_accounts,
+        )
 
     async def place_perp_order(
         self,
@@ -455,29 +443,26 @@ class UtpMangoAccount(UtpAccount):
         mango_authority_pk, _ = await self.authority()
         remaining_accounts = await self.get_observation_accounts()
 
-        if self._config.mango.group_pk is None:
-            raise Exception("Mango group cannot be none.")
-        else:
-            return make_cancel_perp_order_ix(
-                CancelPerpOrderArgs(
-                    order_id=order_id,
-                    invalid_id_ok=invalid_id_ok,
-                ),
-                CancelPerpOrderAccounts(
-                    marginfi_account=self._marginfi_account.pubkey,
-                    marginfi_group=self._marginfi_account.group.pubkey,
-                    authority=self._program.provider.wallet.public_key,
-                    mango_authority=mango_authority_pk,
-                    mango_account=self.address,
-                    mango_program=self._config.mango.program_id,
-                    mango_group=self._config.mango.group_pk,
-                    mango_perp_market=perp_market.address,
-                    mango_bids=perp_market.bids_address,
-                    mango_asks=perp_market.asks_address,
-                ),
-                self._client.program_id,
-                remaining_accounts,
-            )
+        return make_cancel_perp_order_ix(
+            CancelPerpOrderArgs(
+                order_id=order_id,
+                invalid_id_ok=invalid_id_ok,
+            ),
+            CancelPerpOrderAccounts(
+                marginfi_account=self._marginfi_account.pubkey,
+                marginfi_group=self._marginfi_account.group.pubkey,
+                authority=self._program.provider.wallet.public_key,
+                mango_authority=mango_authority_pk,
+                mango_account=self.address,
+                mango_program=self._config.mango.program_id,
+                mango_group=self._config.mango.group_pk,
+                mango_perp_market=perp_market.address,
+                mango_bids=perp_market.bids_address,
+                mango_asks=perp_market.asks_address,
+            ),
+            self._client.program_id,
+            remaining_accounts,
+        )
 
     async def cancel_perp_order(
         self,
@@ -505,8 +490,6 @@ class UtpMangoAccount(UtpAccount):
         """[Internal]"""
         utp_authority_pk, _ = await self.authority()
 
-        if self._config.mango.group_pk is None:
-            raise Exception("Mango group cannot be none.")
         utp_account_pk, _ = get_mango_account_pda(
             self._config.mango.group_pk,
             utp_authority_pk,
