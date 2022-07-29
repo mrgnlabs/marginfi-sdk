a01dd88170ff9095162777d30702df6cbc635040
man0s <95379755+losman0s@users.noreply.github.com>
2022-07-30 01:35:44 +0800

    more cpi examples

--- rust/examples/example-cpi/programs/example-cpi/src/lib.rs
+++ rust/examples/example-cpi/programs/example-cpi/src/lib.rs
@@ -1,7 +1,8 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::Token;
 use marginfi_cpi;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("7zJZFQtYZ7Q3YKC3FFkof9QWUs3LVpN9aCF25VMCYtsR");
 
 #[program]
 pub mod example_cpi {
@@ -18,6 +19,72 @@ pub mod example_cpi {
             },
         );
         marginfi_cpi::cpi::init_marginfi_account(ctx)?;
+        Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.marginfi_program.to_account_info(),
+            marginfi_cpi::cpi::accounts::MarginDepositCollateral {
+                marginfi_account: ctx.accounts.marginfi_account.to_account_info(),
+                marginfi_group: ctx.accounts.marginfi_group.to_account_info(),
+                signer: ctx.accounts.signer.to_account_info(),
+                funding_account: ctx.accounts.funding_account.to_account_info(),
+                token_vault: ctx.accounts.token_vault.to_account_info(),
+                token_program: ctx.accounts.token_program.to_account_info(),
+            },
+        );
+        marginfi_cpi::cpi::margin_deposit_collateral(cpi_ctx, amount)?;
+
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.marginfi_program.to_account_info(),
+            marginfi_cpi::cpi::accounts::MarginWithdrawCollateral {
+                marginfi_account: ctx.accounts.marginfi_account.to_account_info(),
+                marginfi_group: ctx.accounts.marginfi_group.to_account_info(),
+                signer: ctx.accounts.signer.to_account_info(),
+                margin_bank_authority: ctx.accounts.margin_bank_authority.to_account_info(),
+                receiving_token_account: ctx.accounts.receiving_token_account.to_account_info(),
+                margin_collateral_vault: ctx.accounts.token_vault.to_account_info(),
+                token_program: ctx.accounts.token_program.to_account_info(),
+            },
+        );
+        marginfi_cpi::cpi::margin_withdraw_collateral(cpi_ctx, amount)?;
+
+        Ok(())
+    }
+
+    pub fn deposit_and_withdraw(ctx: Context<DepositAndWithdraw>, amount: u64) -> Result<()> {
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.marginfi_program.to_account_info(),
+            marginfi_cpi::cpi::accounts::MarginDepositCollateral {
+                marginfi_account: ctx.accounts.marginfi_account.to_account_info(),
+                marginfi_group: ctx.accounts.marginfi_group.to_account_info(),
+                signer: ctx.accounts.signer.to_account_info(),
+                funding_account: ctx.accounts.user_token_account.to_account_info(),
+                token_vault: ctx.accounts.token_vault.to_account_info(),
+                token_program: ctx.accounts.token_program.to_account_info(),
+            },
+        );
+        marginfi_cpi::cpi::margin_deposit_collateral(cpi_ctx, amount)?;
+
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.marginfi_program.to_account_info(),
+            marginfi_cpi::cpi::accounts::MarginWithdrawCollateral {
+                marginfi_account: ctx.accounts.marginfi_account.to_account_info(),
+                marginfi_group: ctx.accounts.marginfi_group.to_account_info(),
+                signer: ctx.accounts.signer.to_account_info(),
+                margin_bank_authority: ctx.accounts.margin_bank_authority.to_account_info(),
+                receiving_token_account: ctx.accounts.user_token_account.to_account_info(),
+                margin_collateral_vault: ctx.accounts.token_vault.to_account_info(),
+                token_program: ctx.accounts.token_program.to_account_info(),
+            },
+        );
+        marginfi_cpi::cpi::margin_withdraw_collateral(cpi_ctx, amount - 1)?;
+
         Ok(())
     }
 }
@@ -38,4 +105,88 @@ pub struct InitializeAccount<'info> {
     signer: Signer<'info>,
 
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    /// CHECK: TODO
+    pub marginfi_program: AccountInfo<'info>,
+
+    /// CHECK: TODO
+    #[account(mut)]
+    pub marginfi_account: AccountInfo<'info>,
+
+    /// CHECK: TODO
+    #[account(mut)]
+    pub marginfi_group: AccountInfo<'info>,
+
+    pub signer: Signer<'info>,
+
+    /// CHECK: TODO
+    #[account(mut)]
+    pub funding_account: AccountInfo<'info>,
+
+    /// CHECK: TODO
+    #[account(mut)]
+    pub token_vault: AccountInfo<'info>,
+
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    /// CHECK: TODO
+    pub marginfi_program: AccountInfo<'info>,
+
+    /// CHECK: TODO
+    #[account(mut)]
+    pub marginfi_account: AccountInfo<'info>,
+
+    /// CHECK: TODO
+    #[account(mut)]
+    pub marginfi_group: AccountInfo<'info>,
+
+    pub signer: Signer<'info>,
+
+    /// CHECK: TODO
+    pub margin_bank_authority: AccountInfo<'info>,
+
+    /// CHECK: TODO
+    #[account(mut)]
+    pub receiving_token_account: AccountInfo<'info>,
+
+    /// CHECK: TODO
+    #[account(mut)]
+    pub token_vault: AccountInfo<'info>,
+
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct DepositAndWithdraw<'info> {
+    /// CHECK: TODO
+    pub marginfi_program: AccountInfo<'info>,
+
+    /// CHECK: TODO
+    #[account(mut)]
+    pub marginfi_account: AccountInfo<'info>,
+
+    /// CHECK: TODO
+    #[account(mut)]
+    pub marginfi_group: AccountInfo<'info>,
+
+    pub signer: Signer<'info>,
+
+    /// CHECK: TODO
+    pub margin_bank_authority: AccountInfo<'info>,
+
+    /// CHECK: TODO
+    #[account(mut)]
+    pub user_token_account: AccountInfo<'info>,
+
+    /// CHECK: TODO
+    #[account(mut)]
+    pub token_vault: AccountInfo<'info>,
+
+    pub token_program: Program<'info, Token>,
 }
--- rust/examples/example-cpi/tests/example-cpi.ts
+++ rust/examples/example-cpi/tests/example-cpi.ts
@@ -7,6 +7,10 @@ import {
   MarginfiClient,
   Wallet,
   processTransaction,
+  MarginfiAccount,
+  MarginfiConfig,
+  getBankAuthority,
+  uiToNative,
 } from "@mrgnlabs/marginfi-client";
 import {
   Connection,
@@ -15,25 +19,39 @@ import {
   ConfirmOptions,
 } from "@solana/web3.js";
 import { SYSTEM_PROGRAM_ID } from "@zero_one/client";
+import { airdropCollateral } from "./utils";
+import { AccountInfo, Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
 describe("example-cpi", () => {
   anchor.setProvider(anchor.Provider.env());
+
   const program = anchor.workspace.ExampleCpi as Program<ExampleCpi>;
-  const txOpts: ConfirmOptions = { commitment: "confirmed" };
+  const txOpts: ConfirmOptions = { commitment: "processed" };
+  const connection = new Connection("https://api.devnet.solana.com", txOpts);
+  const wallet = Wallet.local();
+
+  let config: MarginfiConfig;
+  let collateral: Token;
+  let ataAi: AccountInfo;
+  let mfClient: MarginfiClient;
+
+  before(async () => {
+    config = await getConfig(Environment.DEVNET, connection);
+    collateral = new Token(
+      connection,
+      config.collateralMintPk,
+      TOKEN_PROGRAM_ID,
+      wallet.payer
+    );
+    ataAi = await collateral.getOrCreateAssociatedAccountInfo(wallet.publicKey);
+
+    // Setup the client for easy feedback
+    mfClient = await MarginfiClient.fetch(config, wallet, connection, txOpts);
+  });
+
+  let marginfiAccount: MarginfiAccount;
 
   it("initializes a marginfi account", async () => {
-    const connection = new Connection("https://api.devnet.solana.com", txOpts);
-    const wallet = Wallet.local();
-    const config = await getConfig(Environment.DEVNET, connection);
-
-    // Setup the client
-    const mfClient = await MarginfiClient.fetch(
-      config,
-      wallet,
-      connection,
-      txOpts
-    );
-
     const marginfiAccountKeypair = Keypair.generate();
     const createMarginfiAccountIx =
       await mfClient.program.account.marginfiAccount.createInstruction(
@@ -54,13 +72,112 @@ describe("example-cpi", () => {
       createMarginfiAccountIx,
       initMarginfiAccountIx
     );
-    const sig = await processTransaction(program.provider, tx, [
-      marginfiAccountKeypair,
-    ]);
+    const sig = await processTransaction(
+      program.provider,
+      tx,
+      [marginfiAccountKeypair],
+      txOpts
+    );
+
+    marginfiAccount = await mfClient.getMarginfiAccount(
+      marginfiAccountKeypair.publicKey
+    );
     console.log(
       `Marginfi account initialized: ${marginfiAccountKeypair.publicKey} (sig: ${sig})`
     );
+  });
 
-    // console.log("Marginfi account created: %s", marginfiAccount.publicKey);
+  it("deposits and withdraws", async () => {
+    const amount = uiToNative(10);
+
+    await airdropCollateral(
+      mfClient.program.provider,
+      amount.toNumber(),
+      config.collateralMintPk,
+      ataAi.address
+    );
+
+    const [marginBankAuthority] = await getBankAuthority(
+      config.groupPk,
+      config.programId
+    );
+
+    const depositIx = await program.methods
+      .deposit(amount)
+      .accounts({
+        marginfiAccount: marginfiAccount.publicKey,
+        marginfiGroup: mfClient.config.groupPk,
+        marginfiProgram: mfClient.programId,
+        signer: wallet.publicKey,
+        fundingAccount: ataAi.address,
+        tokenVault: mfClient.group.bank.vault,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .instruction();
+
+    const tx1 = new Transaction().add(depositIx);
+    const sig1 = await processTransaction(program.provider, tx1, [], txOpts);
+    console.log(`\nDeposit sig: ${sig1}`);
+
+    await marginfiAccount.reload();
+    console.log(marginfiAccount.toString());
+
+    const withdrawIx = await program.methods
+      .withdraw(amount.subn(1))
+      .accounts({
+        marginfiAccount: marginfiAccount.publicKey,
+        marginfiGroup: mfClient.config.groupPk,
+        marginfiProgram: mfClient.programId,
+        signer: wallet.publicKey,
+        receivingTokenAccount: ataAi.address,
+        tokenVault: mfClient.group.bank.vault,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        marginBankAuthority,
+      })
+      .instruction();
+
+    const tx2 = new Transaction().add(withdrawIx);
+    const sig2 = await processTransaction(program.provider, tx2, [], txOpts);
+    console.log(`\nWithdraw sig: ${sig2}`);
+
+    await marginfiAccount.reload();
+    console.log(marginfiAccount.toString());
+  });
+
+  it("atomically deposits and withdraws", async () => {
+    const amount = uiToNative(10);
+
+    await airdropCollateral(
+      mfClient.program.provider,
+      amount.toNumber(),
+      config.collateralMintPk,
+      ataAi.address
+    );
+
+    const [marginBankAuthority] = await getBankAuthority(
+      config.groupPk,
+      config.programId
+    );
+
+    const depositAndWithdrawIx = await program.methods
+      .depositAndWithdraw(amount)
+      .accounts({
+        marginfiAccount: marginfiAccount.publicKey,
+        marginfiGroup: mfClient.config.groupPk,
+        marginfiProgram: mfClient.programId,
+        signer: wallet.publicKey,
+        userTokenAccount: ataAi.address,
+        marginBankAuthority,
+        tokenVault: mfClient.group.bank.vault,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .instruction();
+
+    const tx = new Transaction().add(depositAndWithdrawIx);
+    const sig = await processTransaction(program.provider, tx);
+    console.log(`DepositAndWithdraw sig: ${sig}`);
+
+    await marginfiAccount.reload();
+    console.log(marginfiAccount.toString());
   });
 });
