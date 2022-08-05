40a49fb4a766b804eb34d24991651255ceb31ebb
man0s <95379755+losman0s@users.noreply.github.com>
2022-08-06 00:41:27 +0800

    reset version

--- python/docs/source/conf.py
+++ python/docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, MRGN Labs"
 author = "MRGN Labs"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.2"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
