23164a9fc4e0bd919781b393750c71adac499f91
Jakob Povšič <jakob.povsic@gmail.com>
2022-09-09 15:22:07 +0800

    updated imports

--- rust/marginfi-cli/src/main.rs
+++ rust/marginfi-cli/src/main.rs
@@ -6,18 +6,11 @@ use solana_sdk::pubkey;
 
 #[tokio::main]
 async fn main() {
-    let config = MarginfiClientConfig {
-        marginfi_program: pubkey!("MRGNWSHaWmz3CPFcYt9Fyh8VDcvLJyy2SCURnMco2bC"),
-        marginfi_group: pubkey!("2FdddfNp6knT5tDjKjFREKUjsFKvAppc61bCyuCrTnj2"),
-        env: Env::MAINNET,
-        rpc_endpoint: env!("RPC_ENDPOINT").to_owned(),
-        wallet: "/Users/jakob/.config/solana/id.json".to_owned(),
-        marginfi_account: None,
-    };
-
-    let address = pubkey!("ANSb3uegManRtrH2kvqzwXc8PxMxNFoY3esSHmjM5hGU");
-
-    let marginfi_client = MarginClient::new(config).await.unwrap();
+    let marginfi_client = MarginClient::new_from_env().await.unwrap();
+    let address = marginfi_client
+        .config
+        .marginfi_account
+        .expect("MARGINFI_ACCOUNT not set");
     let marginfi_account = MarginAccount::load(&marginfi_client, &address)
         .await
         .unwrap();
